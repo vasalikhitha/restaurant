179b90a9b5ba19dd4ca7aa6bfd146d4f3e6ef3dc
vasalikhitha <likhithavasa1077@gmail.com>
2021-05-31 12:29:11 +0530

    initial

---
+++ src/main/java/com/example/demo/RestaurantSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSpringbootApplication.class, args);
+	}
+
+}
